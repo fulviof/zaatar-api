a127be89f9d176afe16c03cea8988bda1ae8e793
fulviof <fanelli0157@gmail.com>
2019-04-16 23:49:23 -0300

    port change

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.get('/', (req, res) => {
 require('./app/routes/photo.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server is listening on port "+port);
 });
